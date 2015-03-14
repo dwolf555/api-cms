0ac17328106f8b551d3d00ab7c7b5274776d2d07
Daniel Wolf <danielrwolf5@gmail.com>
2015-03-14 10:45:41 -0500

    working on migration for users_roles

---
+++ app/migrations/20150314154218_create_users_roles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateUsersRolesTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
+     * Uncomment this method if you would like to use it.
+     *
+    public function change()
+    {
+    }
+    */
+    
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $this->table('users_roles')
+            ->addColumn('user_id', 'integer')
+            ->addColumn('role_id', 'integer')
+            ->addForeignKey('user_id', 'users', 'id', ['on_delete' => 'cascade'])
+            ->addForeignKey('role_id', 'roles', 'id')
+
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+
+    }
+}
